79c909d1ee7d2abf554a2c91f2ae8f27ec0e26ab
Evelyn Soares <65402409+evelynsoares@users.noreply.github.com>
2023-08-23 09:36:42 -0300

    Update beginner.py
    1038

--- beginner.py
+++ beginner.py
@@ -165,3 +165,18 @@ elif N > 50 and N <= 75 :
     print("Intervalo (50,75]")
 elif N > 75 and N <= 100 :
     print("Intervalo (75,100]")
+
+#1038
+code, amount = map(float, input().split())
+
+print("Total: R$ ", end="")
+if code == 1:
+    print("%.2f" %(amount*4))
+elif code == 2:
+    print("%.2f" %(amount*4.5))
+elif code == 3:
+    print("%.2f" %(amount*5))
+elif code == 4:
+    print("%.2f" %(amount*2))
+else:
+    print("%.2f" %(amount*1.5))

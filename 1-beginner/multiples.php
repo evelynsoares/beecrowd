36f87603e23aba127f4a44b222882bd35e4c1460
evelynsoares <soares.evelynp@gmail.com>
2025-02-26 19:20:23 -0300

    adding first php on beecrowd

---
+++ 1-beginner/multiples.php
@@ -0,0 +1,11 @@
+<!-- 1044 -->
+<?php
+    fscanf(STDIN, "%d %d", $a, $b);
+    
+    if(($b % $a) == 0 || ($a % $b) == 0) {
+        echo "Sao Multiplos\n";
+    }
+    else {
+        echo "Nao sao Multiplos\n";
+    }
+?>
